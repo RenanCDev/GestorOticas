f7840e8a05b51118b504d9cd8c199238d8b396e4
RenanMRb <renan.costa.bsi@gmail.com>
2023-12-12 00:52:06 -0300

    preparação do modulo para a função que permitirar cancelar um processo de cadastro ou edição

--- colaborador/colaborador.c
+++ colaborador/colaborador.c
@@ -1,20 +1,3 @@
-/*  ("+=========================================================================+\n")
-    ("|                                                                         |\n")
-    ("|               Universidade Federal do Rio Grande do Norte               |\n")
-    ("|                                                                         |\n")
-    ("|                   Centro de Ensino Superior do Seridó                   |\n")
-    ("|                                                                         |\n")
-    ("|             Aluno -- Renan Missias Rodrigues Alves da Costa             |\n")
-    ("|                                                                         |\n")
-    ("|                    Disciplina DCT1106 -- Programação                    |\n")
-    ("|                                                                         |\n")
-    ("|                  Projeto Sistema de Gestão para Óticas                  |\n")
-    ("|                                                                         |\n")
-    ("|               Developed by @RenanMRb - since august, 2023               |\n")
-    ("|                                                                         |\n")
-    ("+=========================================================================+\n")  */
-
-
 //Inclui todas as importações e declarações necessárias no programa
 #include "../util/all.h"
 
@@ -30,7 +13,9 @@ void modulo_colaborador (void) {
                 case '1':
                     limpa_buffer ();
                     colaborador = cad_colab (); //Cria a struct Colab
-                    gravar_colab (colaborador); //Grava a struct Colab em arquivo
+                    if (colaborador != NULL) {
+                        gravar_colab (colaborador); //Grava a struct Colab em arquivo
+                    }
                     break;
                 case '2':
                     limpa_buffer ();
@@ -48,19 +33,30 @@ Colab* cad_colab (void) {
     char* cpf;
     do {
         cpf = le_cpf ("Cadastro colaborador");
-        if (carregar_colab (cpf) != NULL) {
+        if (!cancel(cpf)) {
+            return NULL;
+        } else if (carregar_colab (cpf) != NULL) {
             tela_erro ("Entrada já cadastrada");
         }
     } while (carregar_colab (cpf) != NULL);
     strcpy(col->cpf, cpf);
     limpa_buffer ();
     char* email = le_email ("Cadastro colaborador");
+    if (!cancel(email)) {
+        return NULL;
+    }
     strcpy(col->email, email);
     limpa_buffer ();
     char* cel = le_cel ("Cadastro colaborador");
+    if (!cancel(cel)) {
+        return NULL;
+    }
     strcpy(col->cel, cel);
     limpa_buffer ();
     char* nome = le_nome ("Cadastro colaborador");
+    if (!cancel(nome)) {
+        return NULL;
+    }
     strcpy(col->nome, nome);
     char* data = inst_data ();
     strcpy(col->data, data);
@@ -81,7 +77,9 @@ Colab* pesq_colab (void) {
     do{
         cpf = le_cpf ("Pesquisa colaborador");
         col = carregar_colab (cpf);
-        if (col == NULL) {
+        if (!cancel(cpf)) {
+            return NULL;
+        } else if (col == NULL) {
              tela_erro ("Cadastro inexistente");
         }
     } while (col == NULL);
@@ -101,9 +99,7 @@ Colab* pesq_colab (void) {
 void gravar_colab (Colab* col) {
     FILE *fp_col;
     fp_col = fopen("dat/colaborador.dat", "ab");
-    do {
-        fwrite(col, sizeof(Colab), 1, fp_col);
-    } while (fwrite(col, sizeof(Colab), 1, fp_col) != 1);
+    fwrite(col, sizeof(Colab), 1, fp_col);
     fclose(fp_col);
     free(col);
 }
@@ -152,16 +148,25 @@ void edit_cad_colab (Colab* col, char op) {
         case '1' :
             limpa_buffer ();
             char* email = le_email ("Cadastro colaborador");
+            if (!cancel(email)) {
+                break;
+            }
             strcpy(col->email, email);
             break;
         case '2' :
             limpa_buffer ();
             char* cel = le_cel ("Cadastro colaborador");
+            if (!cancel(cel)) {
+                break;
+            }
             strcpy(col->cel, cel);
             break;
         case '3' :
             limpa_buffer ();
             char* nome = le_nome ("Cadastro colaborador");
+            if (!cancel(nome)) {
+                break;
+            }
             strcpy(col->nome, nome);
             break;
         case '4' :
